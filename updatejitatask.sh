727acbdee6ec96abaac91e9c44c55627babdc471
vsm <devops.july2.2017@gmail.com>
2020-11-22 13:19:54 +0530

    Create updatejitatask.sh

---
+++ updatejitatask.sh
@@ -0,0 +1,19 @@
+curl --request POST \
+  --url 'https://devopsmar12019.atlassian.net/rest/api/2/issue/QUIC-3/transitions?expand=transitions.fields' \
+  --user 'devops.mar1.2019@gmail.com:wlhokM24ZGIjjYwNXxKXDC67' \
+  --header 'Accept: application/json' \
+  --header 'Content-Type: application/json' \
+  --data '{
+    "update": {
+        "comment": [
+            {
+                "add": {
+                    "body": "Comment added when resolving issue"
+                }
+            }
+        ]
+    },
+    "transition": {
+        "id": "31"
+    }
+}'
