b6914169f11b29b1e7c11189135416a0b9205263
vsm <devops.july2.2017@gmail.com>
2020-11-28 18:05:00 +0530

    Update jira.sh

--- jira.sh
+++ jira.sh
@@ -2,6 +2,8 @@
 JIRA_USER="${1}"
 JIRA_PASS="${2}"
 JIRA_NO="QUIC-3"
+TRANSID="21"
+MESSAGE="Comment added when resolving issue"
 curl --request POST \
   --url "https://devopsmar12019.atlassian.net/rest/api/2/issue/${JIRA_NO}/transitions?expand=transitions.fields" \
   --user "${JIRA_USER}:${JIRA_PASS}" \
@@ -12,12 +14,12 @@ curl --request POST \
         "comment": [
             {
                 "add": {
-                    "body": "Comment added when resolving issue"
+                    "body": "$MESSAGE"
                 }
             }
         ]
     },
     "transition": {
-        "id": "31"
+        "id": "$TRANSID"
     }
 }'
