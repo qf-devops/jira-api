25160a04ce7aa05a783c2d29f8f78619d34e8e84
vsm <devops.july2.2017@gmail.com>
2020-11-28 17:51:28 +0530

    Update jira.sh

--- jira.sh
+++ jira.sh
@@ -3,7 +3,7 @@ JIRA_USER="${1}"
 JIRA_PASS="${2}"
 curl --request POST \
   --url 'https://devopsmar12019.atlassian.net/rest/api/2/issue/QUIC-3/transitions?expand=transitions.fields' \
-  --user '${JIRA_USER}:wlhokM24ZGIjjYwNXxKXDC67' \
+  --user '"${JIRA_USER}":wlhokM24ZGIjjYwNXxKXDC67' \
   --header 'Accept: application/json' \
   --header 'Content-Type: application/json' \
   --data '{
