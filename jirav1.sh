b11185d5190358936d03130205c114cd29ddcb36
vsm <devops.july2.2017@gmail.com>
2020-11-30 15:25:43 +0530

    Update jirav1.sh

--- jirav1.sh
+++ jirav1.sh
@@ -3,8 +3,8 @@
 update_status(){
 JIRA_USER="${1}"
 JIRA_PASS="${2}"
-JIRA_NO="QUIC-3"
-TRANSID="21"
+JIRA_NO="${3}"
+TRANSID="${4}"
 MESSAGE="Comment added when resolving issue"
 curl --request POST \
   --url "https://devopsmar12019.atlassian.net/rest/api/2/issue/${JIRA_NO}/transitions?expand=transitions.fields" \
@@ -33,7 +33,14 @@ y=$(echo $a | awk '{print $4}')
 z=$(echo $a | awk '{print $6}')
 prid=$(echo ${y//#})
 br=$(echo $z | awk -F '/' '{print $2}')
-commits=$(curl -X GET -u 'devops.july2.2017@gmail.com':279a86e8173a7ac5f4fa8aa9d2f3dc12d6ef75f5 https://api.github.com/repos/quickfixtech/jira-api/pulls/3/commits)
+if [[ $br == "release" ]]; then
+    TRANSID="21"
+elif [[ $br == "master" ]]; then
+    TRANSID="31"
+else
+   TRANSID="21"
+fi
+commits=$(curl -X GET -u 'devops.july2.2017@gmail.com':279a86e8173a7ac5f4fa8aa9d2f3dc12d6ef75f5 https://api.github.com/repos/quickfixtech/jira-api/pulls/$prid/commits)
 echo "$commits">file.json
 #jq -r '.commit' file.json
 jq -c '.[]' file.json | while read i; do
@@ -42,6 +49,6 @@ rawjid=$(echo $i | jq -r '.commit.message')
 if [[ $rawjid == *":"* ]]; then
   jid=$(echo $rawjid | awk -F ':' '{print $1}')
   jmessage=$(echo $rawjid | awk -F ':' '{print $2}')
-  update_status $JIRA_USER $JIRA_PASS $jid $jmessage
+  update_status $JIRA_USER $JIRA_PASS $jid $jmessage $TRANSID
 fi
 done
