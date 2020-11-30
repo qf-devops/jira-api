2e745cdda7886394856a204a3c2fd391a2288fb7
vsm <devops.july2.2017@gmail.com>
2020-11-30 15:36:27 +0530

    Update jirav1.sh

--- jirav1.sh
+++ jirav1.sh
@@ -43,9 +43,9 @@ fi
 commits=$(curl -X GET -u 'devops.july2.2017@gmail.com':279a86e8173a7ac5f4fa8aa9d2f3dc12d6ef75f5 https://api.github.com/repos/quickfixtech/jira-api/pulls/$prid/commits)
 echo "$commits">file.json
 #jq -r '.commit' file.json
-jq -c '.[]' file.json | while read i; do
+/usr/bin/jq -c '.[]' file.json | while read i; do
 #     do stuff with $i
-rawjid=$(echo $i | jq -r '.commit.message')
+rawjid=$(echo $i | /usr/bin/jq -r '.commit.message')
 if [[ $rawjid == *":"* ]]; then
   jid=$(echo $rawjid | awk -F ':' '{print $1}')
   jmessage=$(echo $rawjid | awk -F ':' '{print $2}')
